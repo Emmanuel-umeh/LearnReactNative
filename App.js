3a37272821010a954621cff336d203502f33515b
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2020-06-26 16:31:09 +0100

    first comit

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  Alert,
+  Button,
+  TouchableWithoutFeedback,
+  TouchableNativeFeedback,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+  Image,
+} from "react-native";
 
 export default function App() {
+  const press = () => {
+    alert("text pressed");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ marginTop: "10%" }} numberOfLines={1} onPress={press}>
+        {" "}
+        Hello world!, this is a very log text i want to test out how it will
+        look like if truncated
+      </Text>
+      {/* https://picsum.photos/200/300 */}
+
+      {/* for local images */}
+      {/* <Image source={require("./assets/icon.png")} /> */}
+
+      {/* for web images */}
+
+      <TouchableOpacity
+        onPress={() => {
+          alert("image tapped");
+        }}
+      >
+        <Image
+          // blurRadius ={10}
+
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri:
+              "https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg",
+          }}
+        />
+      </TouchableOpacity>
+
+      <TouchableNativeFeedback>
+        <View
+          style={{ height: "20%", width: "100%", backgroundColor: "blue" }}
+        ></View>
+      </TouchableNativeFeedback>
+      <Button
+        title="Click Me Twice"
+        color="orange"
+        onPress={() => {
+          Alert.alert("Mega Title", "button tapped", [{text : "Yes" , onPress : ()=>alert("thank you")}, {text : "no"}]);
+        }}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
